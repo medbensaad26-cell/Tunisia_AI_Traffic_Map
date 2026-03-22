170aa753a3fb2576270f35505427b3bea6317292
mohamed <medbensaad26@gmail.com>
2026-03-22 19:23:47 +0100

    Fix port using python main.py

--- main.py
+++ main.py
@@ -138,5 +138,6 @@ def predict(request: PredictRequest):
     }
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
